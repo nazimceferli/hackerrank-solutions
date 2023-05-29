68747f73b809566f552e621a32933a22a198d8ca
nazimceferli <94118612+nazimceferli@users.noreply.github.com>
2023-05-29 12:09:49 +0400

    phonebook code completed

--- src/phonebook/PhoneBook.java
+++ src/phonebook/PhoneBook.java
@@ -1,7 +1,39 @@
 package phonebook;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class PhoneBook {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Say daxil et:");
+        int n = scan.nextInt();
+        scan.nextLine();
+        Map<String, Integer> map = new HashMap<>(n);
+
+
+        for (int i = 0; i < n; i++) {
+            System.out.println("Enter name:");
+            String name = scan.nextLine();
+            System.out.println("Enter phone num:");
+            String num = scan.nextLine();
+
+            //System.out.println(name + " "+ num);
+            map.put(name, Integer.valueOf(num));
+        }
+
+
+        while (scan.hasNext()) {
+            String search = scan.nextLine();
+
+            if (map.containsKey(search)) {
+                System.out.println(search + "=" + map.get(search));
+            } else {
+                System.out.println("Not found");
+            }
+        }
 
     }
 }
+
